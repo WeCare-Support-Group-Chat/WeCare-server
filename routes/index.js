496c2814eee5cd9b1e5aad4a2721596a0148fc1a
Giselle649 <Gisellesamantha.wong@sd.taylors.edu.my>
2023-11-21 18:36:49 +0700

    register, login, updateFirstTimeColumnPOstgres,  done

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const authentication = require("../middlewares/authentication");
-const authorization = require("../middlewares/authorization");
 const Controller = require("../controllers/controller");
+const authentication = require("../middlewares/authentication");
 
-//PUBLIC
-router.post("/login", Controller.login);
-router.post("/register", Controller.register);
+//GISELLE
+router.post("/register", Controller.registerToPostgresAndCE);
+router.post("/loginToPostgres", Controller.loginToPostgres);
+router.patch("/user", authentication, Controller.updateFirstTimeColumnPostgres);
+// router.post("/loginToCE", authentication, Controller.loginToCE);
+//validation
+//Error handler (part sendiri)
+//API
 
-//Require Authentication
-router.use(authentication);
-router.post("/usergroup", Controller.addGroupToUser);
-router.get("/usergroup", Controller.showGroupOfUser);
-
-//Require Authentication & Authorization
-router.delete("/usergroup/:id", authorization, Controller.deleteGroupOfUser);
+//AKBAR
+//add group di postgres & CE
+//show group ke UI kita
+//delete group di postgres & CE
+//Error handler (part sendiri)
 
 module.exports = router;
