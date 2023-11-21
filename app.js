67c7bc40612953193227aa5b22422ea9c0fd8074
Giselle649 <Gisellesamantha.wong@sd.taylors.edu.my>
2023-11-21 15:05:16 +0700

    done setup

---
+++ app.js
@@ -0,0 +1,22 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const router = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//bikin END POINT/ROUTER
+app.use(router);
+
+//global
+app.use(errorHandler);
+
+module.exports = app;
---
+++ controllers/controller.js
@@ -0,0 +1,40 @@
+const { User, Group, UserGroup } = require("../models");
+
+class Controller {
+  static async login(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async register(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async addGroupToUser(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async showGroupOfUser(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async deleteGroupOfUser(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = Controller;
