cb59e27d044355fc62a39bed70bf16e9d86737c6
Muhammad Rizky Akbar <masakbar2905@gmail.com>
2023-11-21 21:38:13 +0700

    done the router except get usergroup

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
+// const cors = require("cors");
+// const express = require("express");
+// const app = express();
+// const router = require("./routes");
+// const errorHandler = require("./middlewares/errorHandler");
+// const PORT = process.env.PORT;
+
+// app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// //bikin END POINT/ROUTER
+// app.use(router);
+
+// app.listen((_) => {
+//   console.log(`app listening on port ${PORT}`);
+// });
+
+// //global
+// app.use(errorHandler);
 
 const cors = require("cors");
+const router = require("./routes");
 const express = require("express");
 const app = express();
-const router = require("./routes");
-const errorHandler = require("./middlewares/errorHandler");
+const port = 3000;
 
 app.use(cors());
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//bikin END POINT/ROUTER
 app.use(router);
 
-//global
-app.use(errorHandler);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 module.exports = app;
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY;
-
+const SECRET_KEY = "rahsiaaaaaa";
 const signToken = (payload) => {
-  return jwt.sign(payload, SECRET_KEY, {});
+  return jwt.sign(payload, SECRET_KEY);
 };
 
 const decode = (token) => {
